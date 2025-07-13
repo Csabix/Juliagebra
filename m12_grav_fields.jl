33ed84a142676f66e8a7fafa25aa9e50f9563b47
Lszaboo <113188930+Lszaboo@users.noreply.github.com>
2025-07-13 23:20:25 +0200

    Fixed "m12_grav_field.jl" example's syntax! 🪐

--- m12_grav_fields.jl
+++ m12_grav_fields.jl
@@ -6,27 +6,27 @@ App()
 cursor = Point(-15,0,0)
 
 function gravField(fieldHeight,fieldStrengthDistance,u,v,p) 
-    xx = u
-    yy = v
+    x = u
+    y = v
     
-    d  = (xx - x(p))^2
-    d += (yy - y(p))^2
+    d  = (x - p[:x])^2
+    d += (y - p[:y])^2
     d  = sqrt(d)
     d  = d * (1.0/fieldStrengthDistance)
     d  = clamp(1.0-d,0.0,1.0)
     d  = sin(d*pi/2.0)
     
-    zz = (d) * z(p) +
-         (1-d) * (fieldHeight)
+    z = (d) * p[:z] +
+        (1-d) * (fieldHeight)
 
-    return (xx,yy,zz)
+    return (x,y,z)
 end
 
-field1 = ParametricSurface([cursor],50,50,-10.0,10.0,-10.0,10.0) do u,v,p
+field1 = ParametricSurface(50,50,-10.0,10.0,-10.0,10.0,[cursor]) do u,v,p
     return gravField(-5.0,5.0,u,v,p)
 end
 
-field2 = ParametricSurface([cursor],50,50,-10.0,10.0,-10.0,10.0) do u,v,p
+field2 = ParametricSurface(50,50,-10.0,10.0,-10.0,10.0,[cursor]) do u,v,p
     return gravField(5.0,5.0,u,v,p)
 end
 

29f67c7e7b90bf8525234e2c34a6160e293a86ec
Lszaboo <113188930+Lszaboo@users.noreply.github.com>
2026-03-17 17:58:23 +0100

    Changed style of ResetWidget! 🛹

--- src/Widgets/reset_widget.jl
+++ src/Widgets/reset_widget.jl
@@ -13,8 +13,10 @@ mutable struct ResetWidget <: ImGuiWidgetDNA
     _width::Int
     _height::Int
 
+    _padding::Int
+
     function ResetWidget(imgui::ImGuiDNA)
-        new(imgui,ImGuiWidget(),0,0,0,0)
+        new(imgui,ImGuiWidget(),0,0,0,0,0)
     end
 end
 
@@ -27,8 +29,8 @@ function render(self::ResetWidget)
 
     CImGui.PushStyleVar(CImGui.ImGuiStyleVar_WindowRounding,11.0)
     CImGui.PushStyleVar(CImGui.ImGuiStyleVar_FrameRounding,6.0)
-    CImGui.PushStyleVar(CImGui.ImGuiStyleVar_WindowPadding,(6,6))
-    CImGui.PushStyleVar(CImGui.ImGuiStyleVar_ItemSpacing,(0,0))
+    CImGui.PushStyleVar(CImGui.ImGuiStyleVar_WindowPadding,(self._padding,self._padding))
+    #CImGui.PushStyleVar(CImGui.ImGuiStyleVar_ItemSpacing,(0,0))
 
 
     CImGui.Begin("ResetWidget",C_NULL,
@@ -39,38 +41,25 @@ function render(self::ResetWidget)
     
 
     CImGui.PushFont(self._imgui._iconFont,24)
-    
-    ssize = (35,35)
-    
+    buttonSize::Tuple{Int,Int} = floor.((self._width,self._height) .- self._padding*2)
+
     # ? Reset button
-    if (CImGui.Button("\uE863",ssize))
-
-    end
-    CImGui.SameLine()
-    # ? 
-    if (CImGui.Button("\uE868",ssize))
-
-    end
-    # ? 
-    if (CImGui.Button("\uE60E",ssize))
-
-    end
-    CImGui.SameLine()
-    # ? 
-    if (CImGui.Button("\uE51C",ssize))
+    if (CImGui.Button("\uE863",buttonSize))
 
     end
 
     CImGui.PopFont()
     
     CImGui.End()
-    CImGui.PopStyleVar(4)
+    CImGui.PopStyleVar(3)
 end
 
 function resize!(self::ResetWidget,x::Int,y::Int)
     self._posX = 5
     self._posY = 5
 
-    self._width = 82
-    self._height = 82
+    self._width = 40
+    self._height = 40
+
+    self._padding = 5
 end

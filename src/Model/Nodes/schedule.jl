e1edb1d106098282b13962943e2549d2ba684177
Lszaboo <113188930+Lszaboo@users.noreply.github.com>
2026-06-28 14:57:38 +0200

    Changed Shcedule to be Vec of Ints! 💡

--- src/Model/Nodes/schedule.jl
+++ src/Model/Nodes/schedule.jl
@@ -1,44 +1,27 @@
 
 # ? ---------------------------------
-# ! Schedule
+# ! InsertionTopoSubgraph
+# ! OrderedTopoSubgraph
 # ? ---------------------------------
 
 @kwdef mutable struct Schedule
-    _vec::Vector{DependentDNA} = Vector{DependentDNA}()
+    _ids::Vector{Int} = Vector{Int}()
 end
 
-Base.length(self::Schedule) = return length(self._vec)
-Base.getindex(self::Schedule, idx::Int) = return self._vec[idx]
-Base.iterate(self::Schedule, state=1) = state<=length(self) ? (return (self._vec[state],state+1)) : (return nothing)
+Base.length(self::Schedule) = return length(self._ids)
+Base.getindex(self::Schedule, idx::Int)::Int = return self._ids[idx]
+Base.iterate(self::Schedule, state=1) = state<=length(self) ? (return (self._ids[state],state+1)) : (return nothing)
+get_ids(self::Schedule)::Vector{Int} = return self._ids
+get_set(self::Schedule)::Set{Int} = return Set(self._ids)
 
-function enchain!(self::Schedule,item::DependentDNA)
-    @assert (length(self)>0) ? (getGraphID(item)>getGraphID(self._vec[end])) : true "IDs must be increasing!"
-    push!(self._vec,item)
+function enchain!(self::Schedule, id::Int)
+    @assert (length(self)>0) ? (id>self._ids[end]) : true "IDs must be increasing!"
+    push!(self._ids,id)
 end
 
-function enchain!(self::Schedule,item::SubjectDNA)
-    observer = _Subject_(item)._observer
-    @assert !isnothing(observer) "Observer of Subject can't be nothing!"
-    @assert (length(self)>0) ? (getGraphID(item)>getGraphID(self._vec[end])) : true "IDs must be increasing!"
-    push!(self._vec,item)
-end
-
-dependentsOf(self::Schedule) = return self._vec
-
-function evalChain(self::Schedule)
-    @invokelatest _evalChain(self)
-end
-
-function _evalChain(self::Schedule)
-    for item in dependentsOf(self)
-        beforeNodeEval(item)
-        onNodeEval(item)
-        afterNodeEval(item)
-    end
-    
-    #for item in observersOf(self)
-    #    postGraphEval(item)
-    #end 
+function enchain!(self::Schedule, item::DependentDNA)
+    id::Int = getGraphID(item)
+    enchain!(self,id)
 end
 
 function Base.merge(s1::Schedule, s2::Schedule)::Schedule
@@ -49,20 +32,18 @@ function Base.merge(s1::Schedule, s2::Schedule)::Schedule
     len2 = length(s2)
 
     while (idx1<=len1) && (idx2<=len2)        
-        d1 = s1[idx1]
-        d2 = s2[idx2]
-        id1 = getGraphID(d1)
-        id2 = getGraphID(d2)
+        id1 = s1[idx1]
+        id2 = s2[idx2]
         
         if (id1 < id2)
-            enchain!(s,d1)
+            enchain!(s,id1)
             idx1+=1
         elseif (id1 > id2)
-            enchain!(s,d2)
+            enchain!(s,id2)
             idx2+=1
         else # ? id1 == id2
-            @assert d1 === d2 "Equal id must mean same Dependent!"
-            enchain!(s,d1)
+            @assert id1 === id2 "IDs must be equal!"
+            enchain!(s,id1)
             idx1+=1
             idx2+=1
         end
--- test/model_tests.jl
+++ test/model_tests.jl
@@ -4,51 +4,109 @@
 
     mutable struct MockDependent <: SUT.DependentDNA
         _dependent::SUT.Dependent
-        _tag::Int
 
-        function MockDependent(callback::Function, dependents::Vector{<:SUT.DependentDNA}, tag::Int)
+        function MockDependent(callback::Function, dependents::Vector{<:SUT.DependentDNA})
             dependent = SUT.Dependent(callback, dependents)
-            new(dependent, tag)
+            new(dependent)
         end
 
-        function MockDependent(dependents::Vector{<:SUT.DependentDNA}, tag::Int)
-            return MockDependent(() -> (return nothing), dependents, tag)
+        function MockDependent(dependents::Vector{<:SUT.DependentDNA})
+            return MockDependent(() -> (return nothing), dependents)
         end
 
-        function MockDependent(tag::Int)
-            return MockDependent(Vector{SUT.DependentDNA}(), tag)
+        function MockDependent()
+            return MockDependent(Vector{SUT.DependentDNA}())
         end
     end
 
     SUT._Dependent_(self::MockDependent)::SUT.Dependent = return self._dependent
-    SUT.onNodeEval(self::MockDependent) = return nothing
+    SUT.onNodeEval(::MockDependent) = return nothing
 
-    function _buildall(model::SUT.Model)
-        for i in 1:4
+    function _buildall(model::SUT.Model, amount::Int)
+        for i in 1:amount
             node = take!(model._builder._in)
             SUT._build(model, node)
         end
     end
 
     @testset verbose=true "Construction Tests" begin
-        model = SUT.Model()        
+        @testset "Diamond" begin
+            model = SUT.Model()        
         
-        node1 = MockDependent(1)
-        node2 = MockDependent([node1],2)
-        node3 = MockDependent([node1],3)
-        node4 = MockDependent([node2, node3], 4)
+            node1 = MockDependent()
+            node2 = MockDependent([node1])
+            node3 = MockDependent([node1])
+            node4 = MockDependent([node2, node3])
 
-        SUT.build!(model, node1)
-        SUT.build!(model, node2)
-        SUT.build!(model, node3)
-        SUT.build!(model, node4)
+            SUT.build!(model, node1)
+            SUT.build!(model, node2)
+            SUT.build!(model, node3)
+            SUT.build!(model, node4)
 
-        _buildall(model)
+            _buildall(model,4)
 
-        @test Set(SUT.getSchedule(node1)._vec) == Set([node2, node3, node4])
-        @test Set(SUT.getSchedule(node2)._vec) == Set([node4])
-        @test Set(SUT.getSchedule(node3)._vec) == Set([node4])
-        @test isempty(SUT.getSchedule(node4))
+            @test SUT.get_set(SUT.getSchedule(node1)) == Set([2, 3, 4])
+            @test SUT.get_set(SUT.getSchedule(node2)) == Set([4])
+            @test SUT.get_set(SUT.getSchedule(node3)) == Set([4])
+            @test isempty(SUT.getSchedule(node4))
+        end
+
+        @testset "DAG" begin
+            model = SUT.Model()        
+        
+            node1 = MockDependent()
+            node2 = MockDependent([node1])
+            node3 = MockDependent([node1])
+            node4 = MockDependent([node2])
+            node5 = MockDependent([node2])
+            node6 = MockDependent([node3])
+            node7 = MockDependent([node3])
+            
+            SUT.build!(model, node1)
+            SUT.build!(model, node2)
+            SUT.build!(model, node3)
+            SUT.build!(model, node4)
+            SUT.build!(model, node5)
+            SUT.build!(model, node6)
+            SUT.build!(model, node7)
+
+            _buildall(model,7)
+
+            @test SUT.get_set(SUT.getSchedule(node1)) == Set([2,3,4,5,6,7])
+            @test SUT.get_set(SUT.getSchedule(node2)) == Set([4,5])
+            @test SUT.get_set(SUT.getSchedule(node3)) == Set([6,7])
+            @test isempty(SUT.getSchedule(node4))
+            @test isempty(SUT.getSchedule(node5))
+            @test isempty(SUT.getSchedule(node6))
+            @test isempty(SUT.getSchedule(node7))
+        end
+
+        @testset "Sink" begin
+            model = SUT.Model()        
+        
+            node1 = MockDependent()
+            node2 = MockDependent()
+            node3 = MockDependent()
+            node4 = MockDependent([node1,node2])
+            node5 = MockDependent([node2,node3])
+            node6 = MockDependent([node4,node5])
+            
+            SUT.build!(model, node1)
+            SUT.build!(model, node2)
+            SUT.build!(model, node3)
+            SUT.build!(model, node4)
+            SUT.build!(model, node5)
+            SUT.build!(model, node6)
+
+            _buildall(model,6)
+
+            @test SUT.get_set(SUT.getSchedule(node1)) == Set([4,6])
+            @test SUT.get_set(SUT.getSchedule(node2)) == Set([4,5,6])
+            @test SUT.get_set(SUT.getSchedule(node3)) == Set([5,6])
+            @test SUT.get_set(SUT.getSchedule(node4)) == Set([6])
+            @test SUT.get_set(SUT.getSchedule(node5)) == Set([6])
+            @test isempty(SUT.getSchedule(node6))
+        end
     end
 
     @testset verbose=true "Evaluation Tests" begin

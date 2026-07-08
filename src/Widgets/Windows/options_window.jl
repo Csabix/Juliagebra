c8f0fa78deab0801ac6315eeaf24f9ae1a7acaab
neugulyascsaba <neugulyascsaba@gmail.com>
2026-07-08 10:39:17 +0200

    White background can be toggled

--- src/Widgets/Windows/options_window.jl
+++ src/Widgets/Windows/options_window.jl
@@ -10,12 +10,17 @@ end
 _Window_(self::OptionsWindow)::Window = self._window
 getWindowName(self::OptionsWindow) = "Options"
 
-function setBackground()
-    println("Bg change!")
+function setBackground(self::OptionsWindow, app::AppDNA)
+    openglD::OpenGLData = getOpenGL(app)
+    if (self._whitebg[])
+        glClearColor(1.0f0, 1.0f0, 1.0f0, 1.0f0)
+    else
+        glClearColor(openglD._backgroundCol[1], openglD._backgroundCol[2], openglD._backgroundCol[3], 1.0f0)
+    end
 end
 
-function renderContent(self::OptionsWindow)
+function renderContent(self::OptionsWindow, app::AppDNA)
     if (CImGui.Checkbox("White background", self._whitebg))
-        setBackground()
+        setBackground(self, app)
     end
 end

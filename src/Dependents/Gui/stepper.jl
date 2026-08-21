103431a9853825d3359e01f44fd9d8902d60bbda
Miklecz Mihály <3misi21@gmail.com>
2026-08-21 19:30:51 +0200

    stepper now uses new node_interface

--- src/Dependents/Gui/stepper.jl
+++ src/Dependents/Gui/stepper.jl
@@ -1,144 +1,76 @@
+struct Stepper
+    value::Float64
+    start_value::Float64
+    label::Union{String,Nothing}
+    playing::Bool
 
-# ? ---------------------------------
-# ! Stepper
-# ? ---------------------------------
+    Stepper(value::Float64, label::Union{String,Nothing}) = new(value, value, label, false)
+    Stepper(s::Stepper, value::Float64) = new(value, s.start_value, s.label, s.playing)
+    Stepper(s::Stepper, value::Float64, playing::Bool) = new(value, s.start_value, s.label, playing)
+end
 
-mutable struct StepperDependent <: GuiDependentDNA
-    _dependent::GuiDependent
-    _num::Float64
+convert_callback_entry(stepper::Stepper)::Float64 = stepper.value
 
-    function StepperDependent(callback::Function, dependents::Vector{<:DependentDNA}, label::String)
-        dependent = GuiDependent(callback,dependents,label)
-        new(dependent,0)
+convert_callback_result(stepper::Stepper, result) = Stepper(stepper, Float64(result))
+
+function update(stepper::Stepper, delta_time::Float64)::Tuple{Any,Bool}
+    if stepper.playing
+        value::Float64 = stepper.value + delta_time
+        return Stepper(stepper, value), true
+    else
+        return stepper, false
     end
 end
 
-_GuiDependent_(self::StepperDependent)::GuiDependent = return self._dependent
-
-# YELLOW Thread
-# RED Thread
-onNodeEval(self::StepperDependent) = evalCallbackDp(self)
-
-Base.eltype(dependent::StepperDependent)::DataType = Float64
-
-evalCallbackDpEntry(self::StepperDependent)::Float64 = return self._num
-
-evalCallbackDpReturn(self::StepperDependent,num::Float64) = self._num = num
-
-# ? ---------------------------------
-# ! StepperRenderer
-# ? ---------------------------------
-
-@kwdef struct _StepperData
-    playing::Bool = false
-    opened::Bool = false
-    proposed::Union{Float32,Nothing} = nothing
-    start::Vec2D = Vec2D(0.0,0.0)
-end
-
-mutable struct StepperRenderer <: GuiRendererDNA{StepperDependent}
-    _renderer::GuiRenderer{StepperDependent}
-    _nums::Vector{Float64}
-    _data::Vector{_StepperData}
-    StepperRenderer(imgui::ImGuiDNA) = new(GuiRenderer{StepperDependent}(imgui),Vector{Float64}(),Vector{_StepperData}())
-end
-
-_GuiRenderer_(self::StepperRenderer)::GuiRenderer{StepperDependent} = return self._renderer
-
-title(::StepperRenderer) = return "Stepper"
-addedAll!(::StepperRenderer) = return nothing
-syncAll!(::StepperRenderer) = return nothing
-
-# GREEN Thread
-function _added!(self::StepperRenderer, item::StepperDependent) 
-    push!(self._nums, item._num)
-    push!(self._data, _StepperData())
-end
-
-# GREEN Thread
-function sync!(self::StepperRenderer, item::StepperDependent) 
-    self._nums[getObserverID(item)] = item._num
-end
-
-function update!(self::StepperRenderer, app::AppDNA)
-    m::Model = getModel(app)
-    steppers::Vector{StepperDependent} = getSubjectItems(self)
-
-    for stepperIdx in eachindex(steppers)
-        stepper::StepperDependent = steppers[stepperIdx]
-        data::_StepperData = self._data[stepperIdx]
-
-        if data.playing
-            tt = time() - data.start.x
-            stepper._num = data.start.y + tt
-            schedule(m,stepper)
-
-        elseif !isnothing(data.proposed)
-            stepper._num = Float64(data.proposed)
-            schedule(m,stepper)
-        end
-    end
-end
-
-function render!(self::StepperRenderer, stepper::StepperDependent, app::AppDNA)
+function render_node_gui(stepper::Stepper)::Any
+    global implicitApp
+    app::App = implicitApp
     imgui::ImGuiData = getImGui(app)
-    label::String = getLabel(stepper)
-    stepperIdx::Int = getObserverID(stepper)
-    num::Float32 = self._nums[stepperIdx]
-    data::_StepperData = self._data[stepperIdx]
+    invalidate = false
 
-    playing = data.playing
-    opened = data.opened
-    #proposed = data.proposed
-    #start = data.start
+    playing = stepper.playing
+    playButtonChar = playing ? "\ue034" : "\ue037"
 
-    playButtonChar= playing ? "\ue034" : "\ue037" 
-
-    # ? Sizing here is good enough for now.
     CImGui.PushFont(imgui._iconFont, 21)
     CImGui.PushStyleVar(CImGui.ImGuiStyleVar_FramePadding, CImGui.ImVec2(0, 0))
-    CImGui.Button("\ue8b8") ? opened = !opened : nothing # ? Settings Cog
-    CImGui.SameLine(0.0,5.0)
-    CImGui.Button(playButtonChar) ? playing = !playing : nothing # ? Play Button
-    start = data.playing==false && playing==true ? Vec2D(time(),num) : data.start
+    CImGui.Button("\ue8b8") ? nothing : nothing # Settings Cog (placeholder)
+    CImGui.SameLine(0.0, 5.0)
+    if CImGui.Button(playButtonChar)
+        playing = !playing
+        invalidate = true
+    end
     CImGui.PopStyleVar()
     CImGui.PopFont()
 
-    if label==""
-        CImGui.SameLine(0.0,5.0)
+    if (stepper.label === nothing)
+        CImGui.SameLine(0.0, 5.0)
     else
-        CImGui.SameLine(0.0,5.0)
-        CImGui.Text("$(label)")
-        CImGui.SameLine(0.0,5.0)
+        CImGui.SameLine(0.0, 5.0)
+        CImGui.Text(stepper.label)
+        CImGui.SameLine(0.0, 5.0)
     end
-    
+
     CImGui.SetNextItemWidth(-1)
-    proposed = input1(num,"", Float32(0.1), Float32(1))
-
-    if opened
-        CImGui.Button("Implement Anim Styles!")
-        CImGui.SameLine(0.0,5.0)
-        CImGui.Button("Implement Anim Speed!")
-        CImGui.SameLine(0.0,5.0)
-        CImGui.Button("Implement Value Setting!")
-        CImGui.Spacing()
+    value = input1(Float32(stepper.value), "", Float32(0.1), Float32(1))
+    if value === nothing
+        return Stepper(stepper, stepper.value, playing), invalidate
     end
-
-    self._data[stepperIdx] = _StepperData(playing, opened, proposed, start)
+    value_f::Float32 = value::Float32
+    invalidate |= Float32(stepper.value) != value_f
+    return Stepper(stepper, Float64(value_f), playing), invalidate
 end
 
-# ? ---------------------------------
-# ! Stepper
-# ? ---------------------------------
+Stepper(; label::Union{String,Nothing}=nothing)::NodeHandle = add_node!(Stepper(0.0, label))
+function Stepper(startVal::Real; label::Union{String,Nothing}=nothing)::NodeHandle
+    add_node!(Stepper(Float64(startVal), label))
+end
 
-Stepper(num::Real; label="") =
-Build!(StepperDependent(Vector{DependentDNA}(), label) do 
-    return Float64(num) 
-end)
-
-Stepper(vh::ValueHolderDNA{<:Real}; label="") =
-Build!(StepperDependent([vh], label) do vh
-    return Float64(vh)
-end)
+macro Stepper(callback::Expr, args...)
+    (positional_args, kw_args) = _parse_macro_arguments(tuple(), (:label,), args...)
+    callback = _validate_callback_expr(callback, 0)
+    return _create_ctor_wrapper(callback, __module__, Juliagebra.Stepper,
+                                positional_args, kw_args)
+end
 
 export Stepper
+export @Stepper
